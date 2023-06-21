370488db3334433584a4abcc68541ec5d2bf2938
Riteek Rakesh <riteek7.12@gmail.com>
2023-06-22 01:27:56 +0530

    Part 1 DONE

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from './Header';
+import Form from './Form';
+import { useEffect, useState } from 'react';
+import List from './List';
+// import { toBeRequired } from '@testing-library/jest-dom/matchers';
 function App() {
+  const [reqType, setReqType] = useState('users');
+
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const API_LINK = 'https://jsonplaceholder.typicode.com'
+
+
+  useEffect(
+    ()=>{
+      const fetchItems = async ()=>{
+        try{
+          const response = await fetch(`${API_LINK}/${reqType}`);
+          if(!response.ok) throw Error('Did not recive the data needed.')
+          const data = await response.json();
+          console.log(data)
+          setItems(data);
+        }catch(err){
+          console.log(err)
+        }finally{
+          setIsLoading(false)
+    
+        }
+      }
+
+      fetchItems()
+    },[reqType]
+  )
+  
+  
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Form reqType={reqType} setReqType={setReqType} />
+
+      <main>
+        <List items={items} />
+      </main>
+      <section>
+        {isLoading && <p>Loading Items..</p>}
+        
+      </section>
+        
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>Fetch Data Challenge</div>
+  )
+}
+
+export default Header
